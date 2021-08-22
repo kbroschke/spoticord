63e57dc3c4452540e84c59047cc005515c66b462
Karl Broschke <karl.broschke@web.de>
2021-08-22 22:26:16 +0200

    Removed init code & and enforced style

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,54 @@
 console.log("Loading libraries...");
-import { existsSync, mkdirSync, writeFileSync, readdirSync } from "fs";
+import { readdirSync } from "fs";
 import { spawn } from "child_process";
 import { Client, Collection } from "discord.js";
 import SpotifyWebApi from "spotify-web-api-node";
 import refreshSpotifyToken from "./refreshSpotifyToken";
-import strings from "./strings";
 import discordConfig from "../config/discord.json";
 import spotifyConfig from "../config/spotify.json";
-
-console.log("Loading configs...");
-// create config dir if not exists
-if (!existsSync("../config")) {
-	mkdirSync("../config");
-}
+const strings = require("strings.js");
 
 // load discord config
-console.log("Loading discord config...");
+console.log("Checking discord config...");
 
 if (discordConfig.BOT_TOKEN) {
-	console.log("Discord config loaded successfully!");
+	console.log("Complete!");
 }
 else {
-	console.error("Error loading discord config, BOT_TOKEN is empty.\n" +
-		"Generating default template...");
-	const defaultConfig = {
-		"BOT_TOKEN": "",
-		// 'DISCORD_USER_ID_OF_SPOTIFY_ACCOUNT_OWNER': '',
-	};
-	writeFileSync("../config/discord.json", JSON.stringify(defaultConfig, null, 4));
-	console.log(strings.discord.configNotFound);
+	console.log(strings.discord.configNotFoundRunning);
 	process.exit();
 }
-
-
-let spotifyAPI: SpotifyWebApi;
 
 // load spotify config
-console.log("Loading spotify config...");
+console.log("Checking spotify config...");
 if (spotifyConfig.CLIENT_ID &&
 	spotifyConfig.CLIENT_SECRET &&
+	spotifyConfig.REFRESH_TOKEN &&
 	spotifyConfig.USERNAME &&
-	spotifyConfig.PASSWORD &&
-	spotifyConfig.LIBRESPOT_PATH) {
-	spotifyAPI = new SpotifyWebApi({
-		clientId: spotifyConfig.CLIENT_ID,
-		clientSecret: spotifyConfig.CLIENT_SECRET,
-		redirectUri: "https://example.com/callback",
-	});
-	if (spotifyConfig.REFRESH_TOKEN) {
-		console.log("Spotify config loaded successfully!");
-	}
-	else if (spotifyConfig.AUTH_CODE) {
-		spotifyAPI.authorizationCodeGrant(spotifyConfig.AUTH_CODE).then(
-			function(data) {
-				spotifyConfig.REFRESH_TOKEN = data.body["refresh_token"];
-				writeFileSync("../config/spotify.json", JSON.stringify(spotifyConfig, null, 4));
-				console.log("Successfully updated refresh token!");
-				console.log("Please start the bot again. (I promise, this should be the last time)");
-				process.exit();
-			},
-			function(error) {
-				console.error(error);
-				console.log(
-					"Something went wrong while sending the auth code to Spotify.\n" +
-					"Please make sure to paste the whole code you got in the redirect URL.\n" +
-					"If you need a new auth code, just remove the current one from the config file and start the bot again.",
-				);
-				process.exit();
-			},
-		);
-	}
-	else {
-		console.log(strings.spotify.noRefreshToken);
-		const scopes = [
-			"user-read-email",
-			"user-read-private",
-			"user-read-playback-state",
-			"user-modify-playback-state",
-			"user-read-currently-playing",
-		];
-		console.log(spotifyAPI.createAuthorizeURL(scopes, ""));
-		process.exit();
-	}
+	spotifyConfig.PASSWORD) {
+	console.log("Complete!");
 }
 else {
-	console.log("Can't load config file! A required parameter is missing. Please check the config file at ../config/spotify.json.");
-
-	// TODO fix
-	console.error("Error loading spotify config. Generating default template...");
-	const defaultConfig = {
-		"CLIENT_ID": "",
-		"CLIENT_SECRET": "",
-		"REFRESH_TOKEN": "",
-		"AUTH_CODE": "",
-		"USERNAME": "",
-		"PASSWORD": "",
-		"LIBRESPOT_PATH": "",
-	};
-	writeFileSync("../config/spotify.json", JSON.stringify(defaultConfig, null, 4));
-	console.log(strings.spotify.configNotFound);
+	console.log(strings.spotify.configNotFoundRunning);
 	process.exit();
 }
 
-// make sure proper prefix-file exists
-if (!existsSync("../config/prefixes.json")) {
-	writeFileSync("../config/prefixes.json", "{}");
-}
+const spotifyAPI = new SpotifyWebApi({
+	clientId: spotifyConfig.CLIENT_ID,
+	clientSecret: spotifyConfig.CLIENT_SECRET,
+	redirectUri: "https://example.com/callback",
+});
 
-type ClientCommands = { commands: Collection<any, any> };
+type ClientCommands = { commands: Collection<String, any> };
 
 console.log("Initiliazing discord client...");
 const client = new Client() as Client & ClientCommands;
 client.commands = new Collection();
 
-const commandFiles = readdirSync("./commands").filter(file => file.endsWith(".js"));
-const eventFilesDiscord = readdirSync("./events/discord").filter(file => file.endsWith(".js"));
-const eventFilesLibrespot = readdirSync("./events/librespot").filter(file => file.endsWith(".js"));
-const eventFilesProcess = readdirSync("./events/process").filter(file => file.endsWith(".js"));
+const commandFiles = readdirSync("./commands").filter((file) => file.endsWith(".js"));
+const eventFilesDiscord = readdirSync("./events/discord").filter((file) => file.endsWith(".js"));
+const eventFilesLibrespot = readdirSync("./events/librespot").filter((file) => file.endsWith(".js"));
+const eventFilesProcess = readdirSync("./events/process").filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -126,17 +58,19 @@ for (const file of commandFiles) {
 for (const file of eventFilesDiscord) {
 	const event = require(`./events/discord/${file}`);
 	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args, client, spotifyAPI));
+		client.once(event.name,
+			(...args) => event.execute(...args, client, spotifyAPI));
 	}
 	else {
-		client.on(event.name, (...args) => event.execute(...args, client, spotifyAPI));
+		client.on(event.name,
+			(...args) => event.execute(...args, client, spotifyAPI));
 	}
 }
 
 console.log("Initializing librespot...");
 
 const librespot = spawn(
-	spotifyConfig.LIBRESPOT_PATH,
+	"../lib/librespot",
 	[
 		"-n", "Librespot",
 		"--device-type", "computer",
@@ -151,12 +85,14 @@ const librespot = spawn(
 
 for (const file of eventFilesLibrespot) {
 	const event = require(`./events/librespot/${file}`);
-	librespot.on(event.name, (...args) => event.execute(...args));
+	librespot.on(event.name,
+		(...args) => event.execute(...args));
 }
 
 for (const file of eventFilesProcess) {
 	const event = require(`./events/process/${file}`);
-	process.on(event.name, (...args) => event.execute(...args, client, librespot));
+	process.on(event.name,
+		(...args) => event.execute(...args, client, librespot));
 }
 
 librespot.stderr.pipe(process.stdout);
@@ -173,7 +109,7 @@ refreshSpotifyToken.execute(spotifyAPI);
 
 client.login(discordConfig.BOT_TOKEN).then(() => {
 	console.log("Discord login complete.");
-}).catch(error => {
+}).catch((error) => {
 	console.error(error);
 	console.error("Could not connect to Discord! Exiting.");
 	process.exit();
--- src/refreshSpotifyToken.ts
+++ src/refreshSpotifyToken.ts
@@ -1,64 +1,65 @@
-import https from 'https';
-import querystring from 'querystring';
+import https from "https";
+import querystring from "querystring";
 import SpotifyWebApi from "spotify-web-api-node";
-import spotify_config from '../config/spotify.json';
+import spotifyConfig from "../config/spotify.json";
 
 export default {
-	name: 'refreshSpotifyToken',
+	name: "refreshSpotifyToken",
 	execute: (spotifyAPI: SpotifyWebApi) => {
-		console.log('Refreshing Spotify\'s Access Token...');
+		console.log("Refreshing Spotify's Access Token...");
 		const data = querystring.stringify({
-			'grant_type': 'refresh_token',
-			'refresh_token': spotify_config.REFRESH_TOKEN,
+			"grant_type": "refresh_token",
+			"refresh_token": spotifyConfig.REFRESH_TOKEN,
 		});
-		const auth = 'Basic ' + Buffer.from(`${spotify_config.CLIENT_ID}:${spotify_config.CLIENT_SECRET}`).toString('base64');
+		const auth = "Basic " + Buffer.from(`${spotifyConfig.CLIENT_ID}:${spotifyConfig.CLIENT_SECRET}`).toString("base64");
 		const options = {
-			hostname: 'accounts.spotify.com',
+			hostname: "accounts.spotify.com",
 			port: 443,
-			path: '/api/token',
-			method: 'POST',
+			path: "/api/token",
+			method: "POST",
 			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': data.length,
-				'Authorization': auth,
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Content-Length": data.length,
+				"Authorization": auth,
 			},
 		};
 
-		const req = https.request(options, res => {
-			let response = '';
+		const req = https.request(options, (res) => {
+			let response = "";
 
-			res.on('data', chunk => {
+			res.on("data", (chunk) => {
 				response += chunk;
 			});
 
-			res.on('end', () => {
+			res.on("end", () => {
 				if (res.statusCode == 200) {
-					spotifyAPI.setAccessToken(JSON.parse(response).access_token);
-					console.log('Successfully updated Spotify Access Token!');
+					spotifyAPI.setAccessToken(
+						JSON.parse(response).access_token);
+					console.log("Successfully updated Spotify Access Token!");
 					spotifyAPI.getMe().then(
-						function(spotify_api_data) {
-							console.log('Authenticated with Spotify Api as:', spotify_api_data.body.email);
+						function(spotifyApiData) {
+							console.log("Authenticated with Spotify Api as:", spotifyApiData.body.email);
 						},
 						function(error) {
-							console.error('--- ERROR INITIALIZING SPOTIFY WEB API ---\n', error);
+							console.error("--- ERROR INITIALIZING SPOTIFY WEB API ---\n", error);
 						},
 					);
 				}
 				else {
 					console.error(
-						'--- SPOTIFY API RESPONSE ERROR ---\n',
+						"--- SPOTIFY API RESPONSE ERROR ---\n",
 						`HTTPS status code: ${res.statusCode}\n`,
-						'Response body:\n' + response);
+						"Response body:\n" + response);
 				}
 			});
 
-			res.on('error', error => {
+			res.on("error", (error) => {
 				console.error(error);
 			});
 		});
 
-		req.on('error', error => {
-			console.error('--- HTTPS ERROR ---\n' + error);
+		req.on("error", (error) => {
+			console.error("--- HTTPS ERROR ---\n" + error);
 		});
 
 		req.write(data);
