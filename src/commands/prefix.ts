f43533e368cc9c71d12cbab491db537206ff5fb1
Karl Broschke <karl.broschke@web.de>
2021-08-24 22:24:09 +0200

    Made it typesafe.

--- src/commands/prefix.ts
+++ src/commands/prefix.ts
@@ -1,37 +1,56 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const prefixes = require('../../config/prefixes.json');
+import { Message, MessageEmbed } from "discord.js";
+import { readFileSync, writeFileSync } from "fs";
 
-const embed = new Discord.MessageEmbed().setColor('#1DB954');
+const embed = new MessageEmbed().setColor("#1DB954");
 
 module.exports = {
-	name: 'prefix',
-	description: 'Sets new prefix. If no argument is given it shows the current prefix.',
-	execute(message, args) {
-		let prefix;
+	name: "prefix",
+	description: "Sets new prefix. If no argument is given it shows the current prefix.",
+	execute(message: Message, args: string[]) {
+		// message-event already performs check that we're not a DM
+		if (!message.guild) {return;}
+
+		let prefixes;
+		try {
+			prefixes = JSON.parse(
+				readFileSync("../../config/prefixes.json").toString());
+		}
+		catch (error) {
+			console.error(error);
+			return;
+		}
+
+
+		let prefix: string;
 		if (message.guild.id in prefixes) {
 			prefix = prefixes[message.guild.id];
 		}
 		else {
-			prefix = '$';
+			prefix = "$";
 		}
 
-		if (!args.length) {
-			message.channel.send(embed.setDescription(`Current command prefix is \`${prefix}\``));
+		if (args.length === 0) {
+			message.channel.send(embed.setDescription(
+				`Current command prefix is \`${prefix}\``));
+			return;
 		}
-		else if(!message.member.hasPermission('ADMINISTRATOR')) {
-			message.reply('sorry but you\'re not an administrator.');
+		if (message.member && !message.member.hasPermission("ADMINISTRATOR")) {
+			message.reply("sorry but you're not an administrator.");
+			return;
 		}
-		else {
-			prefix = args.join(' ');
-			prefixes[message.guild.id] = prefix;
-			fs.writeFile('../../config/prefixes.json', JSON.stringify(prefixes, null, 4), error => {
-				if (error) {
-					console.error('--- FS WRITE ERROR ---', error);
-					message.channel.send('Sorry! There was an internal error!');
-				}
-				else {message.channel.send(embed.setDescription(`Command prefix set to \`${prefix}\``));}
-			});
+
+		prefix = args.join(" ");
+		prefixes[message.guild.id] = prefix;
+		try {
+			writeFileSync("../../config/prefixes.json",
+				JSON.stringify(prefixes, null, 4));
 		}
+		catch (error) {
+			console.error("--- FS WRITE ERROR ---", error);
+			message.channel.send("Sorry! There was an internal error!");
+		}
+
+		message.channel.send(embed.setDescription(
+			`Command prefix set to \`${prefix}\``));
 	},
 };
