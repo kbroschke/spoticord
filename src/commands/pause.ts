3950a530bb7b1f93aac5c35667a33ed2ad78306f
Karl Broschke <karl.broschke@web.de>
2021-09-11 18:58:17 +0200

    Removed idea to pause librespot.stdout on pause

--- src/commands/pause.ts
+++ src/commands/pause.ts
@@ -1,4 +1,3 @@
-import { ChildProcessWithoutNullStreams } from "child_process";
 import { Message, MessageEmbed } from "discord.js";
 import SpotifyWebApi from "spotify-web-api-node";
 import { DEVICE_ID } from "../../config/spotify.json";
@@ -8,12 +7,10 @@ const embed = new MessageEmbed().setColor("#1DB954");
 module.exports = {
 	name: "pause",
 	description: "Pauses Spotify playback.",
-	execute(message: Message, args: string[], spotifyAPI: SpotifyWebApi,
-		librespot: ChildProcessWithoutNullStreams) {
+	execute(message: Message, args: string[], spotifyAPI: SpotifyWebApi) {
 		spotifyAPI.pause({ "device_id": DEVICE_ID }).then(
 			function() {
 				message.react("⏸️");
-				// librespot.stdout.pause();
 			},
 			function(error) {
 				console.error("--- ERROR PAUSING PLAYBACK ---\n", error);
