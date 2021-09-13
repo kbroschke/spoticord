a4aad48172b9edf2babccdd97fc138b93652ad1d
Karl Broschke <karl.broschke@web.de>
2021-09-13 17:55:33 +0200

    Pause Spotify playback on disconnect

--- src/events/discord/voiceStateUpdate.ts
+++ src/events/discord/voiceStateUpdate.ts
@@ -1,8 +1,11 @@
 import { Client, Collection, GuildMember, VoiceState } from "discord.js";
+import SpotifyWebApi from "spotify-web-api-node";
+import { DEVICE_ID } from "../../../config/spotify.json";
 
 module.exports = {
 	name: "voiceStateUpdate",
-	execute(oldState: VoiceState, newState: VoiceState, client: Client) {
+	execute(oldState: VoiceState, newState: VoiceState, client: Client,
+		spotifyAPI: SpotifyWebApi) {
 		if (!client.user) {
 			console.error("Discord client is not logged in!");
 			return;
@@ -14,6 +17,8 @@ module.exports = {
 		if (oldState.id == client.user.id) {
 			// the bot left the voice channel
 			if (newState.channel == null) {
+				// TODO: handle error?
+				spotifyAPI.pause({ device_id: DEVICE_ID });
 				return;
 			}
 			// the bot joined a voice channel
@@ -36,7 +41,8 @@ module.exports = {
 		const clientUser = channelMembers.get(client.user.id);
 		if (clientUser && channelMembers.size === 1) {
 			clientUser.voice.connection?.disconnect();
-			// TODO pause Spotify
+			// pause Spotify TODO: handle error?
+			spotifyAPI.pause({ device_id: DEVICE_ID });
 		}
 	},
 };
