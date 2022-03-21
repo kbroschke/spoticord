f7330bd519398f62f961d64b732c1c0aa42bdc7c
Karl Broschke <karl.broschke@web.de>
2022-03-21 16:18:25 +0100

    Added librespot compile script, Updated dependencies

---
+++ scripts/librespot.sh
@@ -0,0 +1,10 @@
+mkdir lib
+cd ./lib
+git clone https://github.com/librespot-org/librespot.git
+cd librespot
+cargo build --release --no-default-features
+cp target/release/librespot ../librespot-release
+chmod -R +w .git/
+cd ..
+rm -R librespot/
+mv librespot-release librespot
