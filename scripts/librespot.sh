af3c9b39e00bb831f1fd562453aafad22fccab8e
Karl Broschke <karl.broschke@web.de>
2022-03-22 18:01:14 +0100

    Improve librespot compile script
    credits @thoerni

--- scripts/librespot.sh
+++ scripts/librespot.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-rm -R lib
+rm -rf lib/
 mkdir lib
 cd ./lib
 git clone https://github.com/librespot-org/librespot.git
 cd librespot
 cargo build --release --no-default-features
 cp target/release/librespot ../librespot-release
-chmod -R +w .git/
 cd ..
-rm -R librespot/
+rm -rf librespot/
 mv librespot-release librespot
