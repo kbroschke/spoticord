83e9f8ae19e215a2c8739e09232f7b13356e0a56
Karl Broschke <karl.broschke@web.de>
2022-03-23 15:46:49 +0100

    Fix global types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,15 +1,16 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-var */
 import { AudioPlayer } from "@discordjs/voice";
-import { ChildProcess } from "child_process";
+import { ChildProcessWithoutNullStreams } from "child_process";
 import { Client } from "discord.js";
 import SpotifyWebApi from "spotify-web-api-node";
+import { ClientCommands } from "./command";
 
 declare global {
     var spotifyAPI: SpotifyWebApi;
     var player: AudioPlayer;
-    var librespot: ChildProcess;
-    var client: Client;
+    var librespot: ChildProcessWithoutNullStreams;
+    var client: Client & ClientCommands;
 }
 
 export { };
