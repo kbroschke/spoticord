57b830d361e3e312728c0ddcd538eee2e0e7a5db
Karl Broschke <karl.broschke@web.de>
2022-03-26 22:34:00 +0100

    Add full api roundtrip latency to ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,19 +1,29 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction, MessageEmbed } from "discord.js";
+import { CommandInteraction, Message, MessageEmbed } from "discord.js";
 import type { Command } from "types/command";
+
+const embed = new MessageEmbed({
+	title: "Pong!",
+	color: "#1DB954",
+});
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("ping")
 		.setDescription("Show bot latency."),
-	execute(interaction: CommandInteraction) {
-		const timeTaken = Date.now() - interaction.createdTimestamp;
-		const embed = new MessageEmbed({
-			title: "Pong!",
-			color: "#1DB954",
-			description: `\`${timeTaken} ms\``,
-		});
-		interaction.reply({ embeds: [embed] });
-		// TODO fetch reply for two-way latency
+	async execute(interaction: CommandInteraction) {
+		const pingEmbed = embed.setDescription("Pinging...");
+		const reply = await interaction.reply({
+			embeds: [pingEmbed],
+			fetchReply: true,
+		}) as Message;
+
+		const timeTaken = reply.createdTimestamp - interaction.createdTimestamp;
+		const heartbeat = interaction.client.ws.ping;
+
+		const resultEmbed = embed.setDescription(
+			`Roundtrip latency: \`${timeTaken}ms\`\n` +
+			`Websocket heartbeat: \`${heartbeat}ms\``);
+		interaction.editReply({ embeds: [resultEmbed] });
 	},
 } as Command;
